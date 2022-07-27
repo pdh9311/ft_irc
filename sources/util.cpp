15f7044e44a8adc1458758663d170dddf724dfcd
minsunki <minsunki@student.42seoul.kr>
2022-07-28 00:10:09 +0900

    isXStr syntax checking util funcs

--- sources/util.cpp
+++ sources/util.cpp
@@ -6,7 +6,7 @@
 /*   By: minsunki <minsunki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/18 15:12:30 by minsunki          #+#    #+#             */
-/*   Updated: 2022/07/27 15:27:25 by minsunki         ###   ########seoul.kr  */
+/*   Updated: 2022/07/28 00:09:52 by minsunki         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,5 +33,70 @@ namespace irc
 		ss << std::setw(3);
 		ss << x;
 		return (ss.str());
+	}
+
+	bool	isLetterStr(const std::string& str)
+	{
+		std::string::const_iterator	it = str.begin();
+
+		while (it != str.end())
+		{
+			if (!(('a' <= *it && *it <= 'z') || ('A' <= *it && *it <= 'Z')))
+				return (false);
+			++it;
+		}
+		return (true);
+	}
+
+	bool	isDigitStr(const std::string& str)
+	{
+		std::string::const_iterator	it = str.begin();
+
+		while (it != str.end())
+		{
+			if (!('0' <= *it && *it <= '9'))
+				return (false);
+			++it;
+		}
+		return (true);
+	}
+
+	bool	isUserStr(const std::string& str)
+	{
+		std::string::const_iterator it = str.begin();
+
+		while (it != str.end())
+		{
+			if (*it == 0 || *it == '\r' || *it == '\n' || *it == ' ' || *it == '@')
+				return (false);
+			++it;
+		}
+		return (true);
+	}
+
+	bool	isHexDigitStr(const std::string& str)
+	{
+		std::string::const_iterator	it = str.begin();
+
+		while (it != str.end())
+		{
+			if (!('0' <= *it && *it <= '9') && !('A' <= *it && *it <= 'F'))
+				return (false);
+			++it;
+		}
+		return (true);
+	}
+
+	bool	isSpecialStr(const std::string& str)
+	{
+		std::string::const_iterator	it = str.begin();
+
+		while (it != str.end())
+		{
+			if (!(0x5b <= *it && *it <= 0x60) && !(0x7b <= *it && *it <= 0x7d))
+				return (false);
+			++it;
+		}
+		return (true);
 	}
 }
--- sources/util.hpp
+++ sources/util.hpp
@@ -6,7 +6,7 @@
 /*   By: minsunki <minsunki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/18 15:12:40 by minsunki          #+#    #+#             */
-/*   Updated: 2022/07/25 15:01:39 by minsunki         ###   ########seoul.kr  */
+/*   Updated: 2022/07/28 00:08:39 by minsunki         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,9 @@ namespace irc
 {
 	int			stoi(const std::string& str);
 	std::string	to_string(const int& x);
+	bool		isLetterStr(const std::string& str);
+	bool		isDigitStr(const std::string& str);
+	bool		isUserStr(const std::string& str);
+	bool		isHexDigitStr(const std::string& str);
+	bool		isSpecialStr(const std::string& str);
 }
